a95d76f804d8a72f688885488923300cf738dbbe
im-victor-mendez <victor.mmc.pl@hotmail.com>
2024-06-30 13:24:54 -0600

    test: Implemented testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest"/>
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), tsConfigPaths()],
+	test: {
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: './vitest.setup.ts',
+	},
+});
